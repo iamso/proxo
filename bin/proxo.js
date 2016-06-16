5a050005fbfe3958016b92e142feafc2f96a8955
iamso <so@dev.so>
2016-06-16 15:46:37 +0200

    optimize output

--- bin/proxo.js
+++ bin/proxo.js
@@ -9,14 +9,22 @@ var port = process.env.PORT || argv.p || argv._[1] || 0;
 var host = (argv.h || argv._[0] || 'localhost');
 
 function callback(host, port) {
-  var message = `\nProxy for ${colors.underline(host)} from:\n\n`;
+  var message = `\nProxy for ${colors.underline(host)}:\n\n`;
+  var ip;
+  var internal;
+  var external;
   Object.keys(ifaces).forEach(function (dev) {
     ifaces[dev].forEach(function(details) {
-      if (details.family === 'IPv4' && !details.internal) {
-        message += colors.green.bold(`${details.address}:${port}\n`);
+      if (!ip && details.family === 'IPv4' && !details.internal) {
+        ip = details.address;
       }
+    ;
     });
   });
+  internal = `http://localhost:${port}`;
+  external = `http://${ip}:${port}`;
+  message += colors.bold(`internal: ${colors.green(internal)}\n`);
+  message += colors.bold(`external: ${colors.green(external)}\n`);
   message += `\nPress Ctrl+C to exit`;
   console.log(message);
 }
