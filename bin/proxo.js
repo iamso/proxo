05163696a67385b24b2b7ff34644c11816ff9b7b
iamso <so@dev.so>
2020-02-12 13:39:45 +0100

    add qr code with external url

--- bin/proxo.js
+++ bin/proxo.js
@@ -3,6 +3,7 @@
 const colors = require('colors');
 const ifaces = require('os').networkInterfaces();
 const cp = require('copy-paste');
+const qrcode = require('qrcode-terminal');
 const proxo = require('../index');
 
 const argv = require('optimist').argv;
@@ -10,10 +11,7 @@ const port = process.env.PORT || argv.p || argv._[1] || 0;
 const host = (argv.h || argv._[0] || 'localhost');
 
 const callback = (host, port) => {
-  let message = `\nProxy for ${colors.underline(host)}:\n\n`;
   let ip;
-  let internal;
-  let external;
 
   Object.keys(ifaces).forEach(dev => {
     ifaces[dev].forEach(details => {
@@ -24,13 +22,18 @@ const callback = (host, port) => {
     });
   });
 
-  internal = `http://localhost:${port}`;
-  external = `http://${ip}:${port}`;
-  message += colors.bold(`internal: ${colors.green(internal)}\n`);
-  message += colors.bold(`external: ${colors.green(external)}\n`);
-  message += `\nPress Ctrl+C to exit`;
-  cp.copy(external);
-  console.log(message);
+  const internal = `http://localhost:${port}`;
+  const external = `http://${ip}:${port}`;
+
+  qrcode.generate(external, {small: true}, qr => {
+    let message = `\nProxy for ${colors.underline(host)}:\n\n`;
+    message += colors.bold(`internal: ${colors.green(internal)}\n`);
+    message += colors.bold(`external: ${colors.green(external)} ${colors.reset.dim('(copied to clipboard)')}\n`);
+    message += qr;
+    message += `\nPress Ctrl+C to exit`;
+    console.log(message);
+    cp.copy(external);
+  });
 }
 
 proxo(host, port, callback);
