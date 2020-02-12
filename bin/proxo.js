64fffc6547010c8f57241035c7ee5bd65f91faf6
iamso <so@dev.so>
2020-02-12 12:09:44 +0100

    clean up and use proper es6

--- bin/proxo.js
+++ bin/proxo.js
@@ -1,27 +1,29 @@
 #! /usr/bin/env node
 
-var colors = require('colors');
-var ifaces = require('os').networkInterfaces();
-var cp = require('copy-paste');
-var proxo = require('../index');
+const colors = require('colors');
+const ifaces = require('os').networkInterfaces();
+const cp = require('copy-paste');
+const proxo = require('../index');
 
-var argv = require('optimist').argv;
-var port = process.env.PORT || argv.p || argv._[1] || 0;
-var host = (argv.h || argv._[0] || 'localhost');
+const argv = require('optimist').argv;
+const port = process.env.PORT || argv.p || argv._[1] || 0;
+const host = (argv.h || argv._[0] || 'localhost');
 
-function callback(host, port) {
-  var message = `\nProxy for ${colors.underline(host)}:\n\n`;
-  var ip;
-  var internal;
-  var external;
-  Object.keys(ifaces).forEach(function (dev) {
-    ifaces[dev].forEach(function(details) {
+const callback = (host, port) => {
+  let message = `\nProxy for ${colors.underline(host)}:\n\n`;
+  let ip;
+  let internal;
+  let external;
+
+  Object.keys(ifaces).forEach(dev => {
+    ifaces[dev].forEach(details => {
       if (!ip && details.family === 'IPv4' && !details.internal) {
         ip = details.address;
       }
     ;
     });
   });
+
   internal = `http://localhost:${port}`;
   external = `http://${ip}:${port}`;
   message += colors.bold(`internal: ${colors.green(internal)}\n`);
--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-(function(){
-  'use strict';
+const http = require('http');
+const request = require('request');
 
-  module.exports = exports = (function() {
-    var http = require('http');
-    var request = require('request');
-    return function(host, port, callback) {
-      if (!/^http(s)?:\/\//i.test(host)) {
-        host = `http://${host}`;
-      }
-      var server = http.createServer(function(req, res) {
-        if (host) {
-          req.pipe(request(host + req.url)).pipe(res)
-        }
-        else {
-          res.writeHead(404);
-          res.end('Not found');
-        }
-      }).listen(port, function() {
-        callback(host, server.address().port);
-      });
-    };
-  })();
+module.exports = (host, port, callback) => {
+  if (!/^http(s)?:\/\//i.test(host)) {
+    host = `http://${host}`;
+  }
 
-})();
+  const server = http.createServer((req, res) => {
+    if (host) {
+      req.pipe(request(host + req.url)).pipe(res)
+    }
+    else {
+      res.writeHead(404);
+      res.end('Not found');
+    }
+  }).listen(port, () => {
+    callback && typeof callback === 'function' && callback(host, server.address().port);
+  });
+};
